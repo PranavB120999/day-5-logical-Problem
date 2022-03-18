66514bcee295050aa6b72b03dd3128c01a5874b0
Pranav <pranavbhople99@gmail.com>
2022-03-18 16:08:15 +0530

    main for even and odd is added

--- Program.cs
+++ Program.cs
@@ -4,34 +4,23 @@ namespace Day5FunctionalProblem
 {
     internal class Program
     {
-        public static void FlipCoin()
+        public static void EvenOdd()
         {
-            Console.WriteLine("Hello, Welcome to Coin Flip Program");
-            int heads = 0;
-            int tails = 0;
-
-            Console.WriteLine("Enter the Coin flipping operation range: ");
-            int count = int.Parse(Console.ReadLine());
-            while (count > 0)
+            Console.WriteLine("Enter a number to check even or odd: ");
+            int num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
             {
-                Random rand = new Random();
-                int randomCheck = rand.Next(2);
-                if (randomCheck == 0)
-                {
-                    heads++;
-                }
-                else
-                {
-                    tails++;
-                }
-                count--;
+                Console.WriteLine("The given number " + num + " Is even");
             }
-            Console.WriteLine("Number of heads is: " + heads);
-            Console.WriteLine("Number of Tails is: " + tails);
+            else
+            {
+                Console.WriteLine("The given number " + num + " Is Odd");
+            }
         }
-        static void Main(string[] args)
+
+        public static void Main(string[] args)
         {
-            Program.FlipCoin();
+            Program.EvenOdd();
         }
     }
 }
